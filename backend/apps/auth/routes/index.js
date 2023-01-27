38818fd6bde73a1b2cb0c4a77d81cb078282cb6c
Joaquim Gomez <joaquim.gomez@bbconsult.co.uk>
2023-01-27 12:26:07 +0000

    Creating router for user api

---
+++ backend/apps/auth/routes/index.js
@@ -0,0 +1,12 @@
+const { Router } = require("express");
+const authRouter = Router();
+
+const userView = require("../views/userView");
+
+authRouter.get("/user/", userView.list);
+authRouter.post("/user/", userView.create);
+authRouter.get("/user/:id/", userView.get);
+authRouter.patch("/user/:id/", userView.update);
+authRouter.delete("/user/:id/", userView.destroy);
+
+module.exports = authRouter;
