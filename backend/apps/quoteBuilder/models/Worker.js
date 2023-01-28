f59cc8377b4026d39f7cfe7df22b7a3610d3a143
Joaquim Gomez <joaquim.gomez@bbconsult.co.uk>
2023-01-28 15:04:32 +0000

    Must move get_day_rate function outside of the model definition for sequelize

--- backend/apps/quoteBuilder/models/Worker.js
+++ backend/apps/quoteBuilder/models/Worker.js
@@ -6,20 +6,21 @@ const Worker = {
     autoIncrement: true,
     primaryKey: true,
   },
-  rate: { // This is hourly rate
+  rate: {
+    // This is hourly rate
     type: DataTypes.DECIMAL(10, 2),
     allowNull: false,
   },
   title: {
     type: DataTypes.STRING,
     allowNull: false,
-  }
+  },
 };
 
-Worker.get_day_reate = function (rate) {
-  return rate * 8 // 8 hours per day
-}
-
 module.exports = (sequelize) => {
-  return sequelize.define("Worker", Worker, { paranoid: true });
+  worker = sequelize.define("Worker", Worker, { paranoid: true });
+  worker.get_day_rate = (rate) => {
+    return rate * 8;
+  };
+  return worker;
 };
