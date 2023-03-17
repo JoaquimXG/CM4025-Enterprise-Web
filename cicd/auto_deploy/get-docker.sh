1f1a8276405d54786bb0b02c0cf35cb795a2c12c
Joaquim Gomez <joaquim.gomez@bbconsult.co.uk>
2023-03-17 16:04:46 +0000

    Updating auto deploy script

--- cicd/auto_deploy/get-docker.sh
+++ cicd/auto_deploy/get-docker.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
+# Installs docker and docker-compose
+
 sudo apt-get remove -y docker docker-engine docker.io containerd runc
 
 sudo apt-get update
 
 sudo apt-get install -y \
-     apt-transport-https \
-     ca-certificates \
-     curl \
-     gnupg \
-     lsb-release
+  apt-transport-https \
+  ca-certificates \
+  curl \
+  gnupg \
+  lsb-release
 
 sudo mkdir -m 0755 -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
 
 sudo apt-get update
 
